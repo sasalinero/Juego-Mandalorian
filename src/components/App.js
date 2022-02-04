9344122b8d4de9975ef17faa35872af4e8ca1733
sasalinero <sasalinero@gmail.com>
2022-02-04 23:34:50 +0100

    remove_images

--- src/components/App.js
+++ src/components/App.js
@@ -7,35 +7,15 @@ import Layout from "./Layout";
 import Pieces from "./Pieces";
 import Game from "./Game";
 import Rules from "./Rules";
-//import data from "./data.json";
+
 import Start from "./Start";
 
 function App() {
-  let grogu = 0;
-  let rana = 3;
-  let huevo = 3;
-  let galleta = 3;
-  let cesped = [0, 0, 0, 0, 0, 0];
-  let dado = ["rana", "huevo", "galleta", "grogu"];
-
-  console.log(dado);
-
   return (
     <div>
       <Routes>
         <Route path="/" element={<Layout />} />
-        <Route
-          path="/game"
-          element={
-            <Game
-              ranaPrueba={rana}
-              huevoPrueba={huevo}
-              galletaPrueba={galleta}
-              cespedPrueba={cesped}
-              dadoPrueba={dado}
-            />
-          }
-        />
+        <Route path="/game" element={<Game />} />
         <Route path="/pieces" element={<Pieces />} />
         <Route path="/rules" element={<Rules />} />
         <Route path="/start" element={<Start />} />
--- src/components/Game.js
+++ src/components/Game.js
@@ -1,8 +1,8 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import "../styles/game.scss";
 import cesped_verde from "../images/cesped.png";
-import yoda from "../images/yoda.jpg";
+import grogu from "../images/yoda.jpg";
 import dado_juego from "../images/dado_pres.png";
 import galleta_azul from "../images/galleta.jpg";
 import huevo_blanco from "../images/huevo.jpg";
@@ -10,23 +10,85 @@ import rana_verde from "../images/rana.jpg";
 import vasija from "../images/vasija.jpg";
 import App from "./App";
 
-const Game = (props) => {
-  let [rana, setRana] = useState(props.ranaPrueba);
-  let [huevo, setHuevo] = useState(props.huevoPrueba);
-  let [galleta, setGalleta] = useState(props.galletaPrueba);
-  let [cesped, setCesped] = useState(props.cespedPrueba);
-  let [dado, setDado] = useState(props.dadoPrueba);
-  let [yoda, setYoda] = (useState = [""]);
-  let [classRana1, setClassRana1] = useState("");
+const Game = () => {
+  const [rana, setRana] = useState(3);
+  const [huevo, setHuevo] = useState(3);
+  const [galleta, setGalleta] = useState(3);
+  const [cesped, setCesped] = useState(6);
+  const [dado, setDado] = useState(["rana", "huevo", "galleta", "grogu"]);
+  const [yoda, setYoda] = useState(0);
 
-  console.log(props.dadoPrueba);
+  const [classRana1, setClassRana1] = useState("");
+  const [classRana2, setClassRana2] = useState("");
+  const [classRana3, setClassRana3] = useState("");
+
+  const [classHuevo1, setClassHuevo1] = useState("");
+  const [classHuevo2, setClassHuevo2] = useState("");
+  const [classHuevo3, setClassHuevo3] = useState("");
+
+  const [classGalleta1, setClassGalleta1] = useState("");
+  const [classGalleta2, setClassGalleta2] = useState("");
+  const [classGalleta3, setClassGalleta3] = useState("");
+
+  const removeRana = () => {
+    if (rana === 3) {
+      setClassRana3("hidden");
+    } else if (rana === 2) {
+      setClassRana2("hidden");
+    } else if (rana === 1) {
+      setClassRana1("hidden");
+    } else if (rana === 0) {
+      setYoda(yoda + 1);
+    }
+    setRana(rana - 1);
+  };
+
+  const removeHuevo = () => {
+    if (huevo === 3) {
+      setClassHuevo3("hidden");
+    } else if (huevo === 2) {
+      setClassHuevo2("hidden");
+    } else if (huevo === 1) {
+      setClassHuevo1("hidden");
+    } else if (huevo === 0) {
+      setYoda(yoda + 1);
+    }
+    setHuevo(huevo - 1);
+  };
+  const removeGalleta = () => {
+    if (galleta === 3) {
+      setClassGalleta3("hidden");
+    } else if (galleta === 2) {
+      setClassGalleta2("hidden");
+    } else if (galleta === 1) {
+      setClassGalleta1("hidden");
+    } else if (galleta === 0) {
+      setYoda(yoda + 1);
+    }
+    setGalleta(galleta - 1);
+  };
+
   const handleClick = () => {
     let imagen = Math.floor(Math.random() * dado.length);
     let resultadoDado = dado[imagen];
 
     console.log(imagen);
     console.log(dado[imagen]);
-    return setClassRana1("hidden");
+
+    switch (resultadoDado) {
+      case "rana":
+        removeRana();
+
+        break;
+      case "huevo":
+        removeHuevo();
+        break;
+      case "galleta":
+        removeGalleta();
+        break;
+      default:
+        break;
+    }
   };
 
   return (
@@ -41,15 +103,39 @@ const Game = (props) => {
       </section>
       <section className="armario">
         <section className="vasija_galleta_azul">
-          <img className="images" src={galleta_azul} alt="galleta_azul" />
-          <img className="images" src={galleta_azul} alt="galleta_azul" />
-          <img className="images" src={galleta_azul} alt="galleta_azul" />
+          <img
+            className={`images ${classGalleta1}`}
+            src={galleta_azul}
+            alt="galleta_azul"
+          />
+          <img
+            className={`images ${classGalleta2}`}
+            src={galleta_azul}
+            alt="galleta_azul"
+          />
+          <img
+            className={`images ${classGalleta3}`}
+            src={galleta_azul}
+            alt="galleta_azul"
+          />
         </section>
 
         <section className="vasija_huevo_blanco">
-          <img className="images" src={huevo_blanco} alt="huevo_blanco" />
-          <img className="images" src={huevo_blanco} alt="huevo_blanco" />
-          <img className="images" src={huevo_blanco} alt="huevo_blanco" />
+          <img
+            className={`images ${classHuevo1}`}
+            src={huevo_blanco}
+            alt="huevo_blanco"
+          />
+          <img
+            className={`images ${classHuevo2}`}
+            src={huevo_blanco}
+            alt="huevo_blanco"
+          />
+          <img
+            className={`images ${classHuevo3}`}
+            src={huevo_blanco}
+            alt="huevo_blanco"
+          />
         </section>
         <section className="vasija_rana_verde">
           <img
@@ -57,13 +143,21 @@ const Game = (props) => {
             src={rana_verde}
             alt="rana_verde"
           />
-          <img className="images" src={rana_verde} alt="rana_verde" />
-          <img className="images" src={rana_verde} alt="rana_verde" />
+          <img
+            className={`images ${classRana2}`}
+            src={rana_verde}
+            alt="rana_verde"
+          />
+          <img
+            className={`images ${classRana3}`}
+            src={rana_verde}
+            alt="rana_verde"
+          />
         </section>
       </section>
       <section>
         {" "}
-        <img className="yoda" src={yoda} alt="babyoda" />
+        <img className="yoda" src={grogu} alt="babyoda" />
         <img className="cesped" src={cesped_verde} alt="cesped_verde" />
         <img className="cesped" src={cesped_verde} alt="cesped_verde" />
         <img className="cesped" src={cesped_verde} alt="cesped_verde" />
